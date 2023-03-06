80aa9489505c35873af2d15e659b8101c3bd1617
xandro2021 <xandrocastro2021@gmail.com>
2023-03-06 15:47:27 -0600

    First event for the hamburguer menu

---
+++ js/menujs.js
@@ -0,0 +1,10 @@
+let header = document.querySelector(".app-bar");
+let hamburger = document.querySelector("#hamburger");
+let menu = document.querySelector(".desktop-menu");
+
+hamburger.addEventListener("click", function(event) {
+  event.stopPropagation();
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("desktop-menu__link");
+  header.classList.toggle("allViewHeight");
+});
