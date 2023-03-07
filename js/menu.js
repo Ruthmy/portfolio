8f7ec3c10606627cbdf4bbc461551adc34fb81f5
Ruthmy <Ruryemo.exe@gmail.com>
2023-03-07 05:44:50 -0600

    First implementation of the mobile menu and its functions

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 let header = document.querySelector(".app-bar");
 let hamburger = document.querySelector("#hamburger");
-let menu = document.querySelector(".desktop-menu");
+let menu = document.querySelector(".mobile-menu");
 
 hamburger.addEventListener("click", function(event) {
   event.stopPropagation();
   menu.classList.toggle("hidden");
-  menu.classList.toggle("desktop-menu__link");
+  //menu.classList.toggle("menu__link");
   header.classList.toggle("allViewHeight");
 });
