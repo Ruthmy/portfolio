1f62aa7684b6b693fcb753da643e3e107c3ff5d0
Ruthmy <Ruryemo.exe@gmail.com>
2023-03-09 15:15:00 -0600

    Added the very first modal function. - Created the desktop modal version. - Added the desktop modal version CSS styles. - Added the open/close function between the cards and modals. - Added the png and svg images for the modal buttons and snapshoot.

--- js/cards.js
+++ js/cards.js
@@ -3,6 +3,7 @@
 //Get the father element by the class name
 const section = document.querySelector('.recent-works__cards');
 
+
 //Create the principal tag for cards
 const card = document.createElement('section')
 card.className = 'recent-works__card';
@@ -12,8 +13,7 @@ card.className = 'recent-works__card';
 section.appendChild(card);
 
 //Create the HTML syntaxis for the card
-card.innerHTML = `
-    
+card.innerHTML = `    
         <div class="recent-works__img">
             <img src="/images/img-plaholder.png" alt="Project">
             <div class="recent-works__info">
@@ -24,9 +24,77 @@ card.innerHTML = `
                     <li class="recent-works__tag">JavaScript</li>
                     <li class="recent-works__tag">html</li>
                 </ul>
-                <button js-modal-target class="recent-works__link" href="#">See Project</button>
+                <button class="recent-works__link" href="#">See Project</button>
             </div>
-        </div>
-    
+        </div>    
 `;
+
+
+
+//This section is to open the modal when the user click on the See Project button
+const openModal = document.querySelector('.recent-works__link');
+openModal.addEventListener('click', ()=> {
+    
+    //Here is the code to created the modal
+    //First goes the overlay
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    //Then comes the actual modal
+    const modal = document.createElement('div');
+    modal.className = 'modal';
+    //Create the HTML syntaxis for the modal
+    modal.innerHTML = `
+        <div class="modal-window">
+            <div class="modal-header">
+                <div><button class="modal__close-button">&times;</button></div>
+                <img src="/images/snapshoot-portfolio.png" alt="Project">
+            </div>
+            <div class="modal-body">
+                <div class="modal-title">
+                    <h3>Keeping track of hundreds of components</h3>
+                    <a class="modal__link link1" href="#">
+                        <span>See Live</span>
+                        <img src="images/see-live-icon.svg" alt="" class="modal-icon">
+                    </a>
+                    <a class="modal__link link2" href="#">
+                        <span>See Source</span>
+                        <img src="images/see-source-icon.svg" alt="" class="modal-icon">
+                    </a>
+                </div>                    
+            </div>
+                <ul class="modal__tags">
+                    <li class="modal__tag">Ruby on rails</li>
+                    <li class="modal__tag">css</li>
+                    <li class="modal__tag">JavaScript</li>
+                    <li class="modal__tag">html</li>
+                </ul>
+                <p class="modal-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
+        </div>
+        <div id="overlay"></div>        
+    `;
+
+    //Modal will be inside the overlay
+    overlay.appendChild(modal);
+    //Overlay will be inside the section
+    section.appendChild(overlay);
+
+    
+    ///////Listener fo the close button ///////////////////
+    const closeButton = document.querySelector('.modal__close-button');
+    closeButton.addEventListener('click', ()=>{
+
+    //Modal will be inside the overlay
+    overlay.removeChild(modal);
+
+    //Overlay will be inside the section
+    section.removeChild(overlay);
+    })
+
+
+})
+
+
+
+
+
 
