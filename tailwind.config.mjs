cb3212f1bc11aae60e4112045ee6f871c2ed3a2e
Ruthmy <ruthmy17@gmail.com>
2024-02-07 06:08:46 -0600

    Modified 'tailwindconfig.msj' - Improved the styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	//	darkMode: 'class',
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  primary: { "50": "#eef2ff", "100": "#e0e7ff", "200": "#c7d2fe", "300": "#a5b4fc", "400": "#818cf8", "500": "#6366f1", "600": "#4f46e5", "700": "#4338ca", "800": "#3730a3", "900": "#312e81", "950": "#1e1b4b" }
+		}
+	  },
 	},
 	plugins: [],
-}
+  }
+  
