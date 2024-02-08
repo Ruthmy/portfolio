3bfc79b4d7fdcde8e9316790d95fe1e73086d5c2
Ruthmy <ruthmy17@gmail.com>
2024-02-08 09:34:43 -0600

    Modified 'astro.config.mjs' - Instaled Vercel adapter for SSR.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ruthmy.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
