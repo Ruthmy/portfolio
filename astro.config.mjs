83db1aff899bcb68987bb1829e5488e4fd58d762
Ruthmy <ruthmy17@gmail.com>
2024-02-08 09:53:42 -0600

    Modified 'astro.config.mjs' - Undo Vercel adapter for SSR.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ruthmy.github.io',
-  integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind()]
 });
